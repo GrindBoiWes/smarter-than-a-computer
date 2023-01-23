fa3dc133ccbccd56bd35041e02fcd67d3f817741
Wes Harrison <wesharrison89@gmail.com>
2023-01-23 01:48:13 -0800

    Copied original repo and rearranged java

---
+++ Assets/questions.js
@@ -0,0 +1,32 @@
+// List of questions and correct answers
+var questions = [
+    {
+        title: "Arrays in Javascript can be used to store ___",
+        choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
+        answer: "Other Arrays"
+    },
+
+    {
+        title: "Commonly used data types DO NOT include",
+        choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+        answer: "Alerts"
+    },
+
+    {
+        title: "How do you write a comment in Javascript?",
+        choices: ["//This is a comment", "<!--This is a comment-->", " *This is a comment", "'This is a comment'"],
+        answer: "//This is a comment"
+    },
+
+    {
+        title: "The first index of an array is _____",
+        choices: ["1", "2", "0", "None of the above"],
+        answer: "0"
+    },
+
+    {
+        title: "In javascript, which of the following is a logical operator?",
+        choices: ["||", "&&", "$$", "@@"],
+        answer: "&&"
+    }
+];
